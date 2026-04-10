1fc37b19e6f382a19f6a50e07297aab405e8c457
thuraichamyvithushan <145326510+thuraichamyvithushan@users.noreply.github.com>
2026-04-10 14:20:18 +0530

    v6 frontend url added

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ const app = express();
 const allowedOrigins = [
     'http://localhost:3000',
     'https://ho-internal-18gh.vercel.app',
-    'https://ho-internal.vercel.app',
+    'https://hointernal.com',
     process.env.ALLOWED_ORIGIN
 ].filter(Boolean);
 
